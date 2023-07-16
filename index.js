70c2d6b2c0fb9e90f92c8ecd5ac3a6c709ad8c9e
Youssef <youssefkaroui6@gmail.comm>
2023-07-16 17:19:16 -0400

    const question on the works

--- index.js
+++ index.js
@@ -1,7 +1,53 @@
 // TODO: Include packages needed for this application
+var inquirer = require('inquirer');
+var fs = require('fs');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+
+inquirer.prompt([
+    /* Pass your questions in here */
+  ])
+  .then((answers) => {
+    // Use user feedback for... whatever!!
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
+// array of questions for user input
+const questions = [
+    {
+        type:"input",
+        name:"title",
+        message:"Please enter the title of your project."
+    },
+    {
+        type:"input",
+        name:"description",
+        message:"Please enter a descrition of your project. "
+    },
+    {
+        type:"input",
+        name:"installation",
+        message:"Please enter your project's installation instruction. "
+    },
+    {
+        type:"input",
+        name:"usage",
+        message:"Please enter your prooject's usage information. "
+    },
+    {
+        type:"input",
+        name:"contributionGuidelines",
+        message:"Please enter guidelines to contribute to your project. "
+    },
+    {
+        type:"input"
+    }
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
