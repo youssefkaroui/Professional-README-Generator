4f3340fc26d42f074f69251eb4df93448669240f
Youssef <youssefkaroui6@gmail.comm>
2023-07-16 17:42:07 -0400

    questions variable is done

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
-// TODO: Include packages needed for this application
+// packages needed for this application
 var inquirer = require('inquirer');
 var fs = require('fs');
+var generateMarkdown= require("./utils/generateMarkdown.js");
 
 
-inquirer.prompt([
-    /* Pass your questions in here */
-  ])
-  .then((answers) => {
-    // Use user feedback for... whatever!!
-  })
-  .catch((error) => {
-    if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else went wrong
-    }
-  });
+
+
+// inquirer.prompt([
+//     /* Pass your questions in here */
+//   ])
+//   .then((answers) => {
+//     // Use user feedback for... whatever!!
+//   })
+//   .catch((error) => {
+//     if (error.isTtyError) {
+//       // Prompt couldn't be rendered in the current environment
+//     } else {
+//       // Something else went wrong
+//     }
+//   });
 // array of questions for user input
 const questions = [
     {
@@ -44,7 +47,26 @@ const questions = [
         message:"Please enter guidelines to contribute to your project. "
     },
     {
-        type:"input"
+        type:"input",
+        name:"testing",
+        message:"Please enter your project's testing instructions."
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "Please select a license for your project.",
+        choices: ["IBM" , "ISC", "Apache", "MIT", "GPL", "BSD", "GNU","Boost","Mozilla"],
+    },
+    {
+        type: "input",
+        name: "Githubusername",
+        message: "Please enter your GitHub-username."
+
+    },
+    {
+        type:"input",
+        name:"emailAddress",
+        message:"Please enter your email address."
     }
 
 ];
