7a4a76169ad84c81576cbdd2a732b033514cd70d
Youssef <youssefkaroui6@gmail.comm>
2023-07-18 13:30:01 -0400

    function rederLicenseSection done

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -44,15 +44,37 @@ function renderLicenseLink(license) {
     case "Boost":
       return "https://www.boost.org/users/license.html";
     case "Mozilla":
-      return "https://www.mozilla.org/en-US/MPL/";
+      return "https://www.mozilla.org/en-US/foundation/licensing/";
     case "None":
       return "";
 
 }
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// returns the license section of README
+
+function renderLicenseSection(license) {
+  switch (license) {
+    case "IBM":
+      return "LI documents set out the detailed licensing terms that apply to individual programs. They are available for programs released after May 1999 and a new document is created for each release (or update) of an IBM program. LI documents are normally provided with IBM software in hardcopy or softcopy form. Acceptance of the terms of the LI document is necessary prior to installation or first use of the software.";
+    case "ISC":
+      return "Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted, provided that the above copyright notice and this permission notice appear in all copies.";
+    case "Apache":
+      return "Subject to the terms and conditions of this License, each Contributor hereby grants to You a perpetual, worldwide, non-exclusive, no-charge, royalty-free, irrevocable copyright license to reproduce, prepare Derivative Works of, publicly display, publicly perform, sublicense, and distribute the Work and such Derivative Works in Source or Object form." ;
+    case "MIT":
+      return "Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions: The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.";
+    case "BSD":
+      return "Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met: 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer. 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.";
+    case "GNU":
+      return "The GNU General Public License is a free, copyleft license for software and other kinds of works. The licenses for most software and other practical works are designed to take away your freedom to share and change the works. By contrast, the GNU General Public License is intended to guarantee your freedom to share and change all versions of a program--to make sure it remains free software for all its users. We, the Free Software Foundation, use the GNU General Public License for most of our software; it applies also to any other work released this way by its authors. You can apply it to your programs, too.";
+    case "Boost":
+      return "Permission is hereby granted, free of charge, to any person or organization obtaining a copy of the software and accompanying documentation covered by this license (the Software) to use, reproduce, display, distribute,execute, and transmit the Software, and to prepare derivative works of the Software, and to permit third-parties to whom the Software is furnished to do so";
+    case "Mozilla":
+      return "Most Mozilla software projects use the MPL, but some have different terms. Detailed information on the licensing of existing code can be found by inspecting its license headers, or by visiting the license information page in the relevant Mozilla software.";
+    case "None":
+      return "";
+
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
